a0b701b097fbf213b87cdd2bcffa35127cb0ede7
Czimer Kristóf <czimer.kristof@outlook.com>
2020-01-28 17:11:03 +0100

    bővítés

--- public/main.js
+++ public/main.js
@@ -5,6 +5,7 @@ var navigationBar = document.getElementById("navigation-bar");
 addNavLink(navigationBar, "index.html", "Főoldal");
 addNavLink(navigationBar, "aboutme.html", "Rólam");
 addNavLink(navigationBar, "works.html", "Munkáim");
+addNavLink(navigationBar, "tutorials.html", "Gyakorló példák");
 
 function addNavLink(navBarElement, page, label){
     var link = document.createElement("a");
